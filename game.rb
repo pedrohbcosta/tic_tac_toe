3360b89c5409450dfa53f80660891ef5cd239a40
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2023-06-01 17:01:01 -0300

    create Player class

--- game.rb
+++ game.rb
@@ -44,3 +44,25 @@ class Board
     ]
   end
 end
+
+class Player
+  attr_reader :marker
+
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def get_spot(board)
+    spot = nil
+    loop do
+      puts 'Enter [0-8]:'
+      spot = gets.chomp.to_i
+      if spot >= 0 && spot <= 8 && board.available_spaces.include?(spot.to_s)
+        board.mark_spot(spot, @marker)
+        break
+      else
+        puts 'Invalid input. Please enter a number between 0 and 8 that has not already been taken.'
+      end
+    end
+  end
+end
