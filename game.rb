a06c8af9ac667613a5c2fb66e2ae54eac7f46cfe
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2023-06-01 17:02:43 -0300

    create ComputerPlayer class

--- game.rb
+++ game.rb
@@ -66,3 +66,92 @@ class Player
     end
   end
 end
+
+class ComputerPlayer
+  attr_reader :marker, :difficulty
+
+  def initialize(marker, difficulty)
+    @marker = marker
+    @difficulty = difficulty
+  end
+
+  def get_spot(board)
+    spot = nil
+    starting_spot = @difficulty == 'hard' ? '4' : '2'
+    loop do
+      if board.available_spaces.include?(starting_spot)
+        spot = starting_spot.to_i
+        board.mark_spot(spot, @marker)
+        break
+      else
+        spot = get_best_move(board)
+        if board.available_spaces.include?(spot.to_s)
+          board.mark_spot(spot, @marker)
+          break
+        end
+      end
+    end
+    spot
+  end
+
+  private
+
+  def get_best_move(board)
+    available_spaces = board.available_spaces
+    other_marker = @marker == 'x' ? '■'.green : 'x'.pink
+
+    # Check if there's an immediate win or block opportunity
+    available_spaces.each do |as|
+      board.mark_spot(as.to_i, @marker)
+      if board.game_is_over?
+        board.mark_spot(as.to_i, as)
+        return as.to_i
+      else
+        board.mark_spot(as.to_i, other_marker)
+        if board.game_is_over?
+          board.mark_spot(as.to_i, as)
+          return as.to_i
+        else
+          board.mark_spot(as.to_i, as)
+        end
+      end
+    end
+
+    # Look ahead and try to predict player's moves
+    if @difficulty == :hard
+      corners = %w[1 3 7 9]
+      edges = %w[2 4 6 8]
+      best_move = nil
+      corners.each do |corner|
+        next unless available_spaces.include?(corner)
+
+        board.mark_spot(corner.to_i, @marker)
+        if board.get_best_move(other_marker) > 0
+          best_move = corner.to_i
+          board.mark_spot(corner.to_i, as)
+          break
+        else
+          board.mark_spot(corner.to_i, as)
+        end
+      end
+      if best_move.nil?
+        edges.each do |edge|
+          next unless available_spaces.include?(edge)
+
+          board.mark_spot(edge.to_i, @marker)
+          if board.get_best_move(other_marker) > 0
+            best_move = edge.to_i
+            board.mark_spot(edge.to_i, as)
+            break
+          else
+            board.mark_spot(edge.to_i, as)
+          end
+        end
+      end
+      return best_move.to_i if best_move
+    end
+
+    # Otherwise, choose a random available space
+    available_spaces.sample.to_i
+  end
+end
